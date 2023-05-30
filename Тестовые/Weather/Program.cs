c1df2a881631a149c0ac7598af79bbddc13c399a
RVstalker <slava_roshchenko@mail.ru>
2023-05-30 13:15:35 +0300

    30

---
+++ Тестовые/Weather/Program.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace Weather
+{
+    public class Program
+    {
+        // Список городов, для которых реализована возможность получения погоды
+        public enum CitiesEnum
+        {
+            Moscow = 1,
+            Berlin,
+            London,
+            Minsk,
+            Paris
+        }
+        public string ChooseCity()
+        {
+            Console.WriteLine("Please choose a name of city");
+            Console.WriteLine("1. Moscow");
+            Console.WriteLine("2. Berlin");
+            Console.WriteLine("3. London");
+            Console.WriteLine("4. Minsk");
+            Console.WriteLine("5. Paris");
+            Console.WriteLine("6. Write other name");
+            Console.WriteLine("------------------------------");
+            Console.Write("Your choosen nuber is: ");
+            int cityNum = 0;
+            string cityName = null;
+
+            while (cityNum < 1 && cityNum > 6)
+            {
+                try
+                {
+                    cityNum = Console.Read();
+                }
+                catch
+                {
+                    Console.WriteLine("\nError: \" You write not exhist number, try again: \" ");
+                    Console.Write("Your choosen nuber is: ");
+                }
+            }
+
+            switch (cityNum)
+            {
+                case (int)CitiesEnum.Moscow:
+                    cityName = "Moscow";
+                    break;
+                case (int)CitiesEnum.Berlin:
+                    cityName = "Berlin";
+                    break;
+                case (int)CitiesEnum.London:
+                    cityName = "London";
+                    break;
+                case (int)CitiesEnum.Minsk:
+                    cityName = "Minsk";
+                    break;
+                case (int)CitiesEnum.Paris:
+                    cityName = "Paris";
+                    break;
+                case 6:
+                    Console.WriteLine(" Please write city name: ");
+                    cityName = Console.ReadLine();
+                    break;
+            }
+            return cityName;
+        }
+
+        // Метод вывода погоды на данный момент
+        public async void WeatherShow(string cityName)
+        {
+
+            // Получаем погоду для выбранного города
+            var weatherData = await GetWeatherNowAsync(cityName);            
+            // Выводим информацию о погоде сейчас
+            Console.WriteLine($"Current weather in {cityName}: {weatherData.Weather[0].Description}, temperature: {weatherData.Main.Temp}°C, humidity: {weatherData.Main.Humidity}%");
+            // Получаем прогноз погоды на 5 дней для выбранного города
+            var forecastData = await GetWeatherAsync(cityName);
+            // Выводим информацию о прогнозе погоды
+            Console.WriteLine($"Weather forecast for {cityName}:");
+
+            for (var i = 0; i <= 32; i++)
+                Console.WriteLine($"{forecastData.List[i].Dt_Txt}: {forecastData.List[i].Weather[0].Description}, temperature: {forecastData.List[i].Main.Temp}°C," +
+                    $" humidity: {forecastData.List[i].Main.Humidity}%");
+        }
+
+        // Получаем погоду для введенного города на данный момент , если данные не корректны запрашиваем ввод снова
+        public async  Task<WeatherData> GetWeatherNowAsync(string cityName)
+        {
+            string json = null;
+            bool isTrue = true;
+
+            using (var webClient = new HttpClient())
+            {
+                while (isTrue)
+                {
+                    try
+                    {
+                        json = await webClient.GetStringAsync($"https://api.openweathermap.org/data/2.5/weather?q={cityName}&units=metric&appid=78dceb86a6d5f1f8a93172f682e96402");
+                    }
+                    catch
+                    {
+                        Console.Write("Something went wrong, write again name of City:");
+                        cityName = Console.ReadLine();
+                    }
+                    finally
+                    {
+                        isTrue = false;
+                    }
+                }
+            }
+
+            return JsonConvert.DeserializeObject<WeatherData>(json);
+        }
+
+        public async  Task<ForecastData> GetWeatherAsync(string cityName)
+        {
+            string json = null;
+            bool isTrue = true;
+
+            using (var webClient = new HttpClient())
+            {
+                while (isTrue)
+                {
+                    try
+                    {
+                        json = await webClient.GetStringAsync($"https://api.openweathermap.org/data/2.5/weather?q={cityName}&units=metric&appid=78dceb86a6d5f1f8a93172f682e96402");
+                    }
+                    catch
+                    {
+                        Console.Write("Something went wrong, write again name of City:");
+                        cityName = Console.ReadLine();
+                    }
+                    finally
+                    {
+                        isTrue = false;
+                    }
+                }
+            }
+
+            return JsonConvert.DeserializeObject<ForecastData>(json);
+        }
+
+        public static void Main(string[] args)
+        {
+            var prog = new Program();
+            prog.WeatherShow(prog.ChooseCity());
+            Console.ReadKey();
+        }
+        public class WeatherData
+        {
+            public MainData Main { get; set; }
+            public Weather[] Weather { get; set; }
+        }
+        public class MainData
+        {
+            public float Temp { get; set; }
+            public int Humidity { get; set; }
+        }
+        public class Weather
+        {
+            public string Description { get; set; }
+        }
+        public class ForecastData
+        {
+            public List<ForecastItem> List { get; set; }
+        }
+        public class ForecastItem
+        {
+            public MainData Main { get; set; }
+            public Weather[] Weather { get; set; }
+            public DateTime Dt_Txt { get; set; }
+        }
+    }
+}
